4f74e76113ab05efcb0241739b1553109bcafc78
Jêrome Rx <jerome+git@rouaix.eu>
2018-06-02 12:07:21 +0200

    mermaid output

---
+++ src/Grooph/MermaidExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Grooph.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grooph
+{
+    public static class MermaidExtensions
+    {
+        /// <summary>
+        /// Mermaid printout
+        /// https://mermaidjs.github.io/
+        /// </summary>
+        /// <param name="graph">Graph</param>
+        /// <param name="toString">ToString() override if given</param>
+        /// <returns>mermaid js format visualisation</returns>
+        public static string GetMermaid(this Graph graph, Func<object, string> toString = null)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("graph TD;");
+
+            toString = toString ?? (o =>
+            {
+                if (o == null) return string.Empty;
+                switch (o)
+                {
+                    case Vertex v: return v.Id.ToString();
+                    case Edge v: return v.Id.ToString();
+                    default: return o.ToString();
+                }
+            });
+
+            string escape(object value) => value.ToString();
+
+            foreach (var vertex in graph.Vertexes)
+            {
+                sb.AppendLine($"    {escape(vertex.Id)};");
+            }
+
+            foreach (var edge in graph.Edges)
+            {
+                sb.AppendLine($"    {escape(edge.From)} -->|{escape(edge.Id)}| {escape(edge.To)};");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ tests/Grooph.Tests/MermaidExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Grooph.Tests
+{
+    public class MermaidExtensionsTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public MermaidExtensionsTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void SimpleGraph()
+        {
+            var g = Assets.GetSimpleGraph();
+            var dot = g.GetMermaid();
+            _output.WriteLine(dot);
+            dot.ShouldBe(
+@"graph TD;
+    a/a1;
+    a/a2;
+    a/a3;
+    a/a4;
+    a/a5;
+    a/a1 -->|x/x12| a/a2;
+    a/a1 -->|x/x12'| a/a2;
+    a/a2 -->|x/x13| a/a3;
+    a/a2 -->|x/x24| a/a4;
+    a/a4 -->|x/x45| a/a5;
+");
+        }
+
+        [Fact]
+        public void TinyLoopGraph()
+        {
+            var g = Assets.GetTinyLoopGraph();
+            var dot = g.GetMermaid();
+            _output.WriteLine(dot);
+            dot.ShouldBe(
+@"graph TD;
+    a/a1;
+    a/a2;
+    a/a1 -->|x/x1| a/a2;
+    a/a2 -->|z/z1| a/a1;
+");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void GenerateLinearGraph(int length)
+        {
+            var graph = Assets.GenerateLinearGraph(length, i => i.ToString());
+            var dot = graph.GetMermaid();
+            _output.WriteLine(dot);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void GenerateHeavyClusterGraph(int vertexCount)
+        {
+            var graph = Assets.GenerateHeavyClusterGraph(vertexCount, i => i.ToString());
+            var dot = graph.GetMermaid();
+            _output.WriteLine(dot);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(10)]
+        [InlineData(50)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void GenerateRandomGraph(int vertexCount)
+        {
+            var graph = Assets.GenerateRandomGraph(vertexCount, i => i.ToString());
+            var dot = graph.GetMermaid();
+            _output.WriteLine(dot);
+        }
+
+    }
+}
